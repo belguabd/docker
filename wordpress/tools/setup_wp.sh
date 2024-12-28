feaa88d1e9d7dd9b33ed7bef9cf16a11ef293957
belguabd <brahim2001elguabdi@gmail.com>
2024-12-28 19:24:41 +0000

    handle some issues

--- wordpress/tools/setup_wp.sh
+++ wordpress/tools/setup_wp.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+sleep 10;
+ping mariadb
+
+mkdir -p /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core download --path=/var/www/html --allow-root
+mv wp-cli.phar /usr/local/bin/wp-cli.phar
+wp-cli.phar core download --path=/var/www/html --allow-root
+
+
+wp-cli.phar config create \
+    --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASS \
+    --dbhost=mariadb:3306 \
+    --path=/var/www/html \
+    --allow-root
+
 
 tail -f /dev/null
