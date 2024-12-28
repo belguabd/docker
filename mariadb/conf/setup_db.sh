17f4eebb42785c955d795d93241c08c5cd2d2480
belguabd <brahim2001elguabdi@gmail.com>
2024-12-28 13:21:25 +0000

    Allow DB user to connect from any host

--- mariadb/conf/setup_db.sh
+++ mariadb/conf/setup_db.sh
@@ -2,9 +2,9 @@
 service mariadb start;
 sleep 2;
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" 
-mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" 
+mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" 
 mysql -u root -e "CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" 
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'localhost';" 
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';" 
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" 
 mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown;
 exec mysqld_safe;
