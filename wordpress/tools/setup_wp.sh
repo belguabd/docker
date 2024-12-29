6b7f4bd09674ade1305178b1cddc441b0b5d08e9
belguabd <brahim2001elguabdi@gmail.com>
2024-12-29 11:27:44 +0000

    Setup WordPress with MariaDB

--- wordpress/tools/setup_wp.sh
+++ wordpress/tools/setup_wp.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 sleep 10;
-ping mariadb
-
-mkdir -p /var/www/html
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp-cli.phar
 wp-cli.phar core download --path=/var/www/html --allow-root
-
-
 wp-cli.phar config create \
     --dbname=$DB_NAME \
     --dbuser=$DB_USER \
@@ -20,6 +12,4 @@ wp-cli.phar config create \
     --dbhost=mariadb:3306 \
     --path=/var/www/html \
     --allow-root
-
-
 tail -f /dev/null
