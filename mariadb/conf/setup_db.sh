8978b4233bbbe886609e4019eff0301622e53797
belguabd <brahim2001elguabdi@gmail.com>
2024-12-29 17:07:00 +0000

    run php-fpm in foreground

--- mariadb/conf/setup_db.sh
+++ mariadb/conf/setup_db.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 service mariadb start;
-sleep 2;
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" 
 mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" 
 mysql -u root -e "CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" 
--- wordpress/tools/setup_wp.sh
+++ wordpress/tools/setup_wp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-sleep 10;
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp-cli.phar
-wp-cli.phar core download --path=/var/www/html --allow-root
-wp-cli.phar config create \
-    --dbname=$DB_NAME \
-    --dbuser=$DB_USER \
-    --dbpass=$DB_PASS \
-    --dbhost=mariadb:3306 \
-    --path=/var/www/html \
-    --allow-root
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp-cli.phar
+# wp-cli.phar core download --path=/var/www/html --allow-root
+# wp-cli.phar config create \
+    # --dbname=$DB_NAME \
+    # --dbuser=$DB_USER \
+    # --dbpass=$DB_PASS \
+    # --dbhost=mariadb:3306 \
+    # --path=/var/www/html \
+    # --allow-root
+mkdir -p /run/php/
+php-fpm7.4 -F -R
+
 tail -f /dev/null
