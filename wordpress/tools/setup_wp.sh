b0189d7e45e5e633db3ac5432593932c49f03791
belguabd <brahim2001elguabdi@gmail.com>
2024-12-30 14:53:48 +0000

    Add WordPress installation script using wp-cli

--- wordpress/tools/setup_wp.sh
+++ wordpress/tools/setup_wp.sh
@@ -2,7 +2,9 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp-cli.phar
+echo "Downloading WordPress..."
 wp-cli.phar core download --path=/var/www/html --allow-root
+echo "Setting up wp-config.php..."
 wp-cli.phar config create \
     --dbname=$DB_NAME \
     --dbuser=$DB_USER \
@@ -10,7 +12,16 @@ wp-cli.phar config create \
     --dbhost=mariadb:3306 \
     --path=/var/www/html \
     --allow-root
+echo "Installing WordPress..."
+wp-cli.phar core install --path=/var/www/html \
+    --url=$SITE_URL \
+    --title=$SITE_TITLE \
+    --admin_user=$ADMIN_USER \
+    --admin_password=$ADMIN_PASS \
+    --admin_email=$ADMIN_EMAIL --allow-root
 
 mkdir -p /run/php/
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
-php-fpm7.4 -F -R
+echo "WordPress installation completed successfully!"
+php-fpm7.4 -F
+
