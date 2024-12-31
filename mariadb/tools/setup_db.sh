b7723f319cadacbda81fb567c0b6889c3d51c520
belguabd <brahim2001elguabdi@gmail.com>
2024-12-31 12:46:38 +0000

    Add user creation commands in setup_wp.sh

--- mariadb/tools/setup_db.sh
+++ mariadb/tools/setup_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 service mariadb start
+sleep 1;
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE} ;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
--- wordpress/tools/setup_wp.sh
+++ wordpress/tools/setup_wp.sh
@@ -2,6 +2,7 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp-cli.phar
+
 echo "Downloading WordPress..."
 wp-cli.phar core download --path=/var/www/html --allow-root
 echo "Setting up wp-config.php..."
@@ -19,6 +20,10 @@ wp-cli.phar core install --path=/var/www/html \
     --admin_user=$ADMIN_USER \
     --admin_password=$ADMIN_PASS \
     --admin_email=$ADMIN_EMAIL --allow-root
+echo "Setting up permalinks..."
+wp-cli.phar user create $ADMIN_USERNAME $ADMIN_EMAIL --role=$ADMIN_ROLE --user_pass=$ADMIN_PASSWORD --path=/var/www/html --allow-root 1>/dev/null
+wp-cli.phar user create $EDITOR_USERNAME $EDITOR_EMAIL --role=$EDITOR_ROLE --user_pass=$EDITOR_PASSWORD --path=/var/www/html --allow-root 1>/dev/null
+
 
 mkdir -p /run/php/
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
