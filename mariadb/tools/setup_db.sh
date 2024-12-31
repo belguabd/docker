f0339bc7a4163c882ff6c2fb975a3fcd0a047b42
belguabd <brahim2001elguabdi@gmail.com>
2024-12-31 10:09:09 +0000

    modify script for database

---
+++ mariadb/tools/setup_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+service mariadb start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE} ;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+service mariadb stop
+mysqld_safe --port=3306 --bind-address=0.0.0.0
