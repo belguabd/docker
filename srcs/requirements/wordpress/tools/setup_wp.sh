80fe3d04f4d071d185991ddb31413ded0d23ab2f
belguabd <brahim2001elguabdi@gmail.com>
2025-01-13 03:46:31 -0500

    Add restart policy 'unless-stopped' to docker-compose.yml

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -3,7 +3,6 @@ sleep 5;
 wp core download --allow-root
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb:3306 --allow-root
 wp core install --url=$SITE_URL --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
-wp user create $ADMIN_USERNAME $ADMIN_USER_EMAIL --role=$ADMIN_ROLE --user_pass=$ADMIN_PASSWORD --allow-root 1>/dev/null
 wp user create $EDITOR_USERNAME $EDITOR_EMAIL --role=$EDITOR_ROLE --user_pass=$EDITOR_PASSWORD --allow-root 1>/dev/null
 wp plugin install redis-cache --activate --allow-root
 wp config set WP_REDIS_HOST redis --allow-root
